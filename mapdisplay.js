50af6263b8e882546a2b609881335b200d02b91f
Zheng Sun <zhensn@gmail.com>
2014-09-09 11:39:48 -0700

    Update mapdisplay.js

--- mapdisplay.js
+++ mapdisplay.js
@@ -74,7 +74,7 @@ function drawRoute(filePath, color) {
         });
         // event listener to show name of route on mouseover
         google.maps.event.addListener(routeObj.polyline, 'mouseover', function(event) {
-            businfo.setContent("<div style='width:200px'>" + routeObj.name + "</div>");
+            businfo.setContent("<div style='width:115px; text-align:center'>" + routeObj.name + "</div>");
             businfo.setPosition(event.latLng);
             businfo.open(window.map);
         });
